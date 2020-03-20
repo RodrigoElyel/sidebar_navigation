24a68904bc8d5e2809965cea554238b950b8c23d
Rodrigo <rodrigoelyelcb@gmail.com>
2020-03-20 01:43:17 -0300

    sidebar + menu item completed

--- lib/ui/sidebar.dart
+++ lib/ui/sidebar.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:rxdart/rxdart.dart';
+import 'menu_item.dart';
 
 class SideBar extends StatefulWidget {
   @override
@@ -61,27 +62,104 @@ class _SideBarState extends State<SideBar>
             duration: _animationDuration,
             top: 0,
             bottom: 0,
-            left: isSidebarOpenedAsync.data ? 0 : 0,
+            left: isSidebarOpenedAsync.data ? 0 : -screenWidth,
             right: isSidebarOpenedAsync.data ? 0 : screenWidth - 45,
             child: Row(
               children: <Widget>[
-                Expanded(child: Container(color: Color(0xFF262AAA))),
+                Expanded(
+                    child: SingleChildScrollView(
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    color: const Color(0xFF262AAA),
+                    child: Column(
+                      children: <Widget>[
+                        SizedBox(
+                          height: 100,
+                        ),
+                        ListTile(
+                          title: Text(
+                            "Rodrigo",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 30,
+                                fontWeight: FontWeight.w800),
+                          ),
+                          subtitle: Text(
+                            "rdg@gmail.com",
+                            style: TextStyle(
+                              color: Color(0xFF1BB5FD),
+                              fontSize: 20,
+                            ),
+                          ),
+                          leading: CircleAvatar(
+                            child: Icon(
+                              Icons.perm_identity,
+                              color: Colors.white,
+                            ),
+                            radius: 40,
+                          ),
+                        ),
+                        Divider(
+                          height: 64,
+                          thickness: 0.5,
+                          color: Colors.white.withOpacity(0.3),
+                          indent: 32,
+                          endIndent: 32,
+                        ),
+                        MenuItem(
+                          icon: Icons.home,
+                          title: "home",
+                        ),
+                        MenuItem(
+                          icon: Icons.person,
+                          title: "My Account",
+                        ),
+                        MenuItem(
+                          icon: Icons.shopping_basket,
+                          title: "My Orders",
+                        ),
+                        MenuItem(
+                          icon: Icons.card_giftcard,
+                          title: "Wishlist",
+                        ),
+                        Divider(
+                          height: 64,
+                          thickness: 0.5,
+                          color: Colors.white.withOpacity(0.3),
+                          indent: 32,
+                          endIndent: 32,
+                        ),
+                        MenuItem(
+                          icon: Icons.settings,
+                          title: "Settings",
+                        ),
+                        MenuItem(
+                          icon: Icons.exit_to_app,
+                          title: "Logout",
+                        ),
+                      ],
+                    ),
+                  ),
+                )),
                 Align(
                   alignment: Alignment(0, -0.9),
                   child: GestureDetector(
                     onTap: () {
                       onIconPressed();
                     },
-                    child: Container(
-                      width: 35,
-                      height: 110,
-                      color: Color(0xFF262AAA),
-                      alignment: Alignment.centerLeft,
-                      child: AnimatedIcon(
-                        icon: AnimatedIcons.menu_close,
-                        progress: _animationController.view,
-                        color: Color(0xFF1BB5FD),
-                        size: 25,
+                    child: ClipPath(
+                      clipper: CustomMenuClipper(),
+                      child: Container(
+                        width: 35,
+                        height: 110,
+                        color: Color(0xFF262AAA),
+                        alignment: Alignment.centerLeft,
+                        child: AnimatedIcon(
+                          icon: AnimatedIcons.menu_close,
+                          progress: _animationController.view,
+                          color: Color(0xFF1BB5FD),
+                          size: 25,
+                        ),
                       ),
                     ),
                   ),
@@ -91,6 +169,37 @@ class _SideBarState extends State<SideBar>
       },
     );
   }
+}
+
+class CustomMenuClipper extends CustomClipper<Path>{
+  @override
+  Path getClip(Size size) {
+    // TODO: implement getClip
+    Paint paint = Paint();
+    paint.color = Colors.white;
+
+    final width = size.width;
+    final height = size.height;
+
+    Path path = Path();
+    path.moveTo(0, 0);
+    path.quadraticBezierTo(0, 8, 10, 16);
+    path.quadraticBezierTo(width-1, height/2 - 20, width, height/2);
+    path.quadraticBezierTo(width+1, height/2 + 20, 10, height-16);
+    path.quadraticBezierTo(0, height-8, 0, height);
+
+
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    // TODO: implement shouldReclip
+    return true;
+  }
+
 }
 
 //
